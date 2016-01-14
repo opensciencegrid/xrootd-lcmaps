5e3824e0ccd22f05ab9d35d28d1b9cb8a80797a0
Brian Bockelman <bbockelm@cse.unl.edu>
2016-01-14 10:36:25 -0600

    Use VOMS to populate entries in sec entity.

--- src/XrdLcmapsKey.cc
+++ src/XrdLcmapsKey.cc
@@ -3,17 +3,23 @@
 #include <sstream>
 #include <iostream>
 
+#include <string.h>
+
 #include <voms/voms_apic.h>
 
 #include <openssl/crypto.h>
+
+#include "XrdLcmapsKey.hh"
 
 /**
  * Build a hash key from the DN and VOMS info.
  */
 std::string
-GetKey(X509 *cert, STACK_OF(X509*) chain)
+GetKey(X509 *cert, STACK_OF(X509*) chain, XrdSecEntity &ent)
 {
     std::stringstream key;
+    std::stringstream grps;
+    bool found_grp = false;
     // Start with the DN
     char *dn = X509_NAME_oneline(X509_get_subject_name(cert), NULL, 0);
     key << dn << "::";
@@ -34,18 +40,27 @@ GetKey(X509 *cert, STACK_OF(X509*) chain)
     {
         struct voms *it = voms_ptr->data[idx];
         if (!it->voname) {continue;}
+        if (!ent.vorg) {ent.vorg = strdup(it->voname);}
         key << it->voname << ":";
         for (int idx2 = 0; it->std[idx2] != NULL; idx2++)
         {
             struct data *it2 = it->std[idx2];
             if (!it2->group) {continue;}
+            if (found_grp) {grps << " ";}
+            else {found_grp = true;}
+            grps << it2->group;
             key << it2->group;
-            if (it2->role) {key << "/Role=" << it2->role;}
+            if (it2->role)
+            {
+                key << "/Role=" << it2->role;
+                ent.role = strdup(it2->role);
+            }
             key << ",";
         }
         key << "::";
     }
     VOMS_Destroy(voms_ptr);
+    ent.grps = strdup(grps.str().c_str());
     return key.str();
 }
 
